18407b25033b1bd6f92ab73e858d15c40db48183
aiamanme <amanme9285@gmail.com>
2023-08-15 23:09:26 +0530

    Animaction responcive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aiamanme.github.io',
   base: '/aiaman',
-  });
+  integrations: [image()]
+});
